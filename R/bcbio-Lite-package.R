bc5745e6e8fdb98d1ce2d1cd48f7f77ccfe1c359
Lorena Pantano <lorena.pantano@gmail.com>
2018-08-14 13:25:10 -0400

    Add reexport of SE methods. And initial qc.

--- R/bcbio-Lite-package.R
+++ R/bcbio-Lite-package.R
@@ -8,5 +8,15 @@
 #' @importFrom dplyr "%>%"
 #' @import rio
 #' @import DESeq2
+#' @importFrom S4Vectors DataFrame
 #'
 "_PACKAGE"
+
+# Alias required for help links in downstream packages
+#' @importFrom SummarizedExperiment colData
+#' @export
+SummarizedExperiment::colData
+
+#' @importFrom SummarizedExperiment rowData
+#' @export
+SummarizedExperiment::rowData
